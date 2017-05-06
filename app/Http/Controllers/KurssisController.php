34800d1b7ac3659651f3374b92f9c0d3e3bf04ff
kiivuna <anu.strommer@gmail.com>
2017-05-07 01:14:22 +0300

    Kuudes

--- app/Http/Controllers/KurssisController.php
+++ app/Http/Controllers/KurssisController.php
@@ -34,69 +34,187 @@ class KurssisController extends Controller
 	    //$data = $request->input();
 	    //return $data;
 	    //$result = DB::select(DB::raw("SELECT * FROM users"));
-	    DB::beginTransaction();
 	    $vastaus = $request->input('vastaus');
 	    $op_vastaus = $request->input('op_vastaus');
 	    $kyselytyyppi = $request->input('kyselytyyppi');
 	    $tehtnro = $request->input('tehtnro');
-	    try {
-		    //if($vastaus == $op_vastaus){
-	    	if(strcasecmp($kyselytyyppi, 'delete') == 0 ){   //kirjaimen koolla ei valia
-	    		if(strcasecmp(preg_replace('/\s+/', '', $vastaus), preg_replace('/\s+/', '', $op_vastaus)) == 0){
-			    	$tehtnro++;
-					return redirect()->back()->with('saatunro', $tehtnro);
-	    		}
-	    		else{
-	    			return redirect()->back()->withErrors([
-    					'message' => 'Tehtävä meni väärin. Kokeile uudestaan.'
-    				])->with('saatunro', $tehtnro);
-	    		}
+	    $yr_nro = $request->input('yr_nro');
+	    $ses_id = $request->input('ses_id');
+	    $opisk_id = $request->input('opisk_id');
+	    $teht_id = $request->input('teht_id');
+	    if($yr_nro < 2){
+		    if($yr_nro != -1 && $op_vastaus == ""){
 
-	    	}
-		    else if(strcasecmp($kyselytyyppi, 'select') == 0 ){
-          	   $result = DB::select($vastaus);
-               DB::rollback();
-               DB::beginTransaction();
-               $yritys =  DB::select($op_vastaus);
-               DB::rollback();
-               if($yritys == $result){
-                  DB::rollback();
-                  //return $op_vastaus;//back();
-                  # Pass the value while redirecting
-                  $tehtnro++;
-                  return redirect()->back()->with('saatunro', $tehtnro);
-               }
-          }
- 		    else if(strcasecmp($kyselytyyppi, 'insert') == 0 ){
-          	   $result = DB::insert($vastaus);
-               DB::rollback();
-               DB::beginTransaction();
-               $yritys =  DB::insert($op_vastaus);
-               DB::rollback();
-               if($yritys == $result){
-                  DB::rollback();
-                  //return $op_vastaus;//back();
-                  # Pass the value while redirecting
-                  $tehtnro++;
-                  return redirect()->back()->with('saatunro', $tehtnro);
-               }
-          }
-		    else{
-		    	DB::rollback();
-    			return redirect()->back()->withErrors([
-					'message' => 'Tehtävä meni väärin. Kokeile uudestaan.'
-				])->with('saatunro', $tehtnro);
-		    	//echo dd($result[0]);
-		    	
+				$yr_nro++;
+		    	return redirect()->back()->withErrors([
+	    			'message' => 'Yritä edes kirjoittaa jotain! Kokeile uudestaan.' 
+	    		])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
 		    }
-		    //echo $result[0]->id;
+		    else if($yr_nro != -1 && substr(preg_replace('/\s+/', '', $op_vastaus), -1) != ";"){
+		    	$yr_nro++;
+		    	return redirect()->back()->withErrors([
+	    			'message' => 'Et kirjoittanut loppuun ; -merkkiä. Kokeile uudestaan.' 
+	    		])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);	
+		    }
+		    else{
+			    try {
+				    //if($vastaus == $op_vastaus){
+			    	if(strcasecmp($kyselytyyppi, 'delete') == 0 ){   //kirjaimen koolla ei valia
+			    		if(strcasecmp(preg_replace('/\s+/', '', $vastaus), preg_replace('/\s+/', '', $op_vastaus)) == 0){
+					    	$tehtnro++;
+					    	$yr_nro = 0;
+							return redirect()->back()->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', false);
+			    		}
+			    		else{
+			    			$yr_nro++;
+			    			return redirect()->back()->withErrors([
+		    					'message' => 'Tehtävä meni väärin. Kokeile uudestaan.'
+		    				])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+			    		}
 
-		} catch(\Illuminate\Database\QueryException $ex){ 
-		  //dd($ex->getMessage()); 
-		  // Note any method of class PDOException can be called on $ex.
-		     return redirect()->back()->withErrors([
-    			'message' => 'Jotain meni pieleen syntaksissa. Kokeile uudestaan.'
-    		])->with('saatunro', $tehtnro);
+			    	}
+				    else if(strcasecmp($kyselytyyppi, 'select') == 0 ){
+		          	   DB::beginTransaction();
+		          	   $result = DB::select($vastaus);
+		               DB::rollback();
+		               DB::beginTransaction();
+		               $yritys =  DB::select($op_vastaus);
+		               DB::rollback();
+		               if($yritys == $result){
+		                  //return $op_vastaus;//back();
+		                  # Pass the value while redirecting
+		                  $tehtnro++;
+		                  $yr_nro = 0;
+		                  return redirect()->back()->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', false);
+		               }
+		               else{
+							$yr_nro++;
+							return redirect()->back()->withErrors([
+								'message' => 'Tehtävä meni väärin. Kokeile uudestaan.'
+							])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+							//echo dd($result[0]);
+				    	
+				    	}
+		          }
+		 		    else if(strcasecmp($kyselytyyppi, 'insert') == 0 ){
+		          	   DB::beginTransaction();
+		          	   $result = DB::insert($vastaus);
+		               DB::rollback();
+		               DB::beginTransaction();
+		               $yritys =  DB::insert($op_vastaus);
+		               DB::rollback();
+		               if($yritys == $result){
+		                  //return $op_vastaus;//back();
+		                  # Pass the value while redirecting
+		                  $tehtnro++;
+		                  $yr_nro = 0;
+		                  return redirect()->back()->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', false);
+		               }
+					    else{
+					    	$yr_nro++;
+			    			return redirect()->back()->withErrors([
+								'message' => 'Tehtävä meni väärin. Kokeile uudestaan.'
+							])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+					    	//echo dd($result[0]); 	
+					    }
+
+		          }
+				    else{
+				    	$yr_nro++;
+		    			return redirect()->back()->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+				    	//echo dd($result[0]);
+				    	
+				    }
+				    //echo $result[0]->id;
+
+				} catch(\Illuminate\Database\QueryException $ex){ 
+				  //dd($ex->getMessage()); 
+				  // Note any method of class PDOException can be called on $ex.
+					$yr_nro++;
+				    return redirect()->back()->withErrors([
+		    			'message' => 'Jotain meni pieleen syntaksissa. Kokeile uudestaan.'
+		    		])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+				}
+			}
+		}
+		else{
+			if($yr_nro != -1 && $op_vastaus == ""){
+				$vaarin_meni = true;
+		    }
+		    else if($yr_nro != -1 && substr(preg_replace('/\s+/', '', $op_vastaus), -1) != ";"){
+				$vaarin_meni = true;
+		    }
+		    else{
+			    try {
+				    //if($vastaus == $op_vastaus){
+			    	if(strcasecmp($kyselytyyppi, 'delete') == 0 ){   //kirjaimen koolla ei valia
+			    		if(strcasecmp(preg_replace('/\s+/', '', $vastaus), preg_replace('/\s+/', '', $op_vastaus)) == 0){
+							$vaarin_meni = false;
+			    		}
+			    		else{
+							$vaarin_meni = true;
+			    		}
+
+			    	}
+				    else if(strcasecmp($kyselytyyppi, 'select') == 0 ){
+		          	   DB::beginTransaction();
+		          	   $result = DB::select($vastaus);
+		               DB::rollback();
+		               DB::beginTransaction();
+		               $yritys =  DB::select($op_vastaus);
+		               DB::rollback();
+		               if($yritys == $result){
+		                  $vaarin_meni = false;
+		               }
+		               else{
+							$vaarin_meni = true;
+							//echo dd($result[0]);
+				    	}
+		          	}
+		 		    else if(strcasecmp($kyselytyyppi, 'insert') == 0 ){
+		          	   DB::beginTransaction();
+		          	   $result = DB::insert($vastaus);
+		               DB::rollback();
+		               DB::beginTransaction();
+		               $yritys =  DB::insert($op_vastaus);
+		               DB::rollback();
+		               if($yritys == $result){
+		                  //return $op_vastaus;//back();
+		                  # Pass the value while redirecting
+						  $vaarin_meni = false;
+		               }
+					    else{
+							$vaarin_meni = true;
+					    	//echo dd($result[0]); 	
+					    }
+
+		          }
+				    else{
+				    	$vaarin_meni = true;
+				    	//echo dd($result[0]);		    	
+				    }
+				    //echo $result[0]->id;
+
+				} catch(\Illuminate\Database\QueryException $ex){ 
+				  //dd($ex->getMessage()); 
+				  // Note any method of class PDOException can be called on $ex.
+					$vaarin_meni = true;
+				}
+			}
+
+			if($vaarin_meni){
+				$yr_nro = -1;
+				$tehtnro++;
+				return redirect()->back()->withErrors([
+					'message' => 'Tehtävä meni väärin. Tässä esimerkkivastaus: '. $vastaus
+				])->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', true);
+			}
+			else{
+				$tehtnro++;
+		        $yr_nro = 0;
+                return redirect()->back()->with('saatunro', $tehtnro)->with('yrityskerta', $yr_nro)->with('vaarin_meni', false);	
+			}
+
 		}
 
 	}
--- resources/views/kurssis/kurssisessiot.blade.php
+++ resources/views/kurssis/kurssisessiot.blade.php
@@ -21,21 +21,132 @@
             $nro = 0;
         ?>
     @endif
+
+
         
     <?php
     $nro = intval(session('saatunro'));
+    $yr_nro = intval(session('yrityskerta'));
+    $vaara = session('vaarin_meni');
     //echo session('nro');
     //$teht = next($tehtavat);
     ?>
 
+
     @if(isset($tehtavat[$nro]))
         <?php
+            //$sessio->id
+            //Auth::user()->id
+            //$tehtavat->id
             $teht = $tehtavat[$nro];
+            $kerta = $yr_nro+1;
+            $tehtavan_nro = $nro +1;
+
         ?>
+        @if($kerta != 0)
+            @if(!empty( DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first() ))
+                <?php
+                    $taulu = DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first();
+                    DB::table('sessiotehtavas')
+                        ->where([
+                            'sessio_id' => $taulu->sessio_id, 
+                            'op_id' => $taulu->op_id,
+                            't_id' => $taulu->t_id,
+                            'yritys' => $taulu->yritys     
+                        ])
+                        ->update([
+                            'lopetus_hetki' => DB::raw('now()'),
+                            'oikein' => !($vaara)
+                        ]);
+                ?>
+            @endif
+            <?php
+                DB::table('sessiotehtavas')->insert([
+                    'sessio_id' => $sessio->id, 
+                    'op_id' => Auth::user()->id,
+                    't_id' => $teht->id,
+                    'aloitus_hetki' => DB::raw('now()'),
+                    'yritys' => $kerta
+                ]);
+            ?>
+        @endif
+
+
+        @if($yr_nro == -1 && $vaara)
+            @if(!empty( DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first() ))
+                <?php
+                    $taulu = DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first();
+                    DB::table('sessiotehtavas')
+                        ->where([
+                            'sessio_id' => $taulu->sessio_id, 
+                            'op_id' => $taulu->op_id,
+                            't_id' => $taulu->t_id,
+                            'yritys' => $taulu->yritys     
+                        ])
+                        ->update([
+                            'lopetus_hetki' => DB::raw('now()'),
+                            'oikein' => !($vaara)
+                        ]);
+                ?>
+            @endif
 
           <li class="list-group-item">
-            Tehtävän kuvaus: 
+            Tietokannan taulut:
+          </li>
+            <img style="width:95px;height:111px" src="http://wiki.scratch.mit.edu/w/images/Scratch_Cat.png" alt="Scratch-klubi" />
+          <li class="list-group-item">
+            Tehtävä meni väärin. Tässä esimerkkivastaus:
+            </br>
+            {{ $tehtavat[$nro-1]->esim_vastaus }}
+          </li>
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('saveproduct') }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <input type="hidden" class="form-control" name="tehtnro" id="tehtnro" value="{{ $nro  }}">
+                </div>
+
+                <div class="form-group">
+                    <input type="hidden" class="form-control" name="yr_nro" id="yr_nro" value="{{ $yr_nro  }}">
+                </div>
+
+                <div class="form-group">
+                    <input type="hidden" class="form-control" name="ses_id" id="ses_id" value="{{ $sessio->id }}">
+                </div>
+
+                <div class="form-group">
+                    <input type="hidden" class="form-control" name="opisk_id" id="opisk_id" value="{{ Auth::user()->id }}">
+                </div>
+
+                <div class="form-group">
+                    <input type="hidden" class="form-control" name="teht_id" id="teht_id" value="{{ $teht->id }}">
+                </div>
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary" name="submit" value="submit">
+                            Jatka seuraavaan tehtävään.
+                        </button>
+                    </div>
+                </div>
+                
+            </form>
+        
+        @else
+          <li class="list-group-item">
+            Tietokannan taulut:
+          </li>
+            <img style="width:95px;height:111px" src="http://wiki.scratch.mit.edu/w/images/Scratch_Cat.png" alt="Scratch-klubi" />
+          
+          <li class="list-group-item">
+            Tehtävä
+            {{ $tehtavan_nro }}:
+            </br>
             {{ $teht->teht_kuvaus }}
+            </br>
+          </li>
+          <li class="list-group-item">
+            Yritys:
+            {{ $kerta }} / 3
             </br>
             Tehtävän esimerkkivastaus:
             {{ $teht->esim_vastaus }}
@@ -68,6 +179,10 @@
                 </div>
 
                 <div class="form-group">
+                    <input type="hidden" class="form-control" name="yr_nro" id="yr_nro" value="{{ $yr_nro  }}">
+                </div>
+
+                <div class="form-group">
                     <div class="col-md-6 col-md-offset-4">
                         <button type="submit" class="btn btn-primary" name="submit" value="submit">
                             Vastaa
@@ -76,10 +191,52 @@
                 </div>
                 @include('layouts.errors')
             </form>
+        @endif
 
     @else
-        <a class="" href='/kurssit'>Loppu! Palaa Omat kurssit -sivulle</a>
+        @if($yr_nro == -1 && $vaara)
+          <li class="list-group-item">
+            Tietokannan taulut:
+          </li>
+            <img style="width:95px;height:111px" src="http://wiki.scratch.mit.edu/w/images/Scratch_Cat.png" alt="Scratch-klubi" />
+          <li class="list-group-item">
+            Tehtävä meni väärin. Tässä esimerkkivastaus:
+            </br>
+            {{ $tehtavat[$nro-1]->esim_vastaus }}
+          </li>
+        @endif
 
+        @if(!empty( DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first() ))
+            <?php
+                $taulu = DB::table('sessiotehtavas')->orderBy('aloitus_hetki', 'desc')->first();
+                DB::table('sessiotehtavas')
+                    ->where([
+                        'sessio_id' => $taulu->sessio_id, 
+                        'op_id' => $taulu->op_id,
+                        't_id' => $taulu->t_id,
+                        'yritys' => $taulu->yritys     
+                    ])
+                    ->update([
+                        'lopetus_hetki' => DB::raw('now()'),
+                        'oikein' => !($vaara),
+                    ]);
+
+                DB::table('loppuneet')->insert([
+                    'sessio_id' => $sessio->id, 
+                    'op_id' => Auth::user()->id,
+                ]);
+            ?>
+         @endif
+            <form class="form-horizontal" role="form" method="GET" action="/kurssit">
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary" name="submit" value="submit">
+                            Jatka Omat kurssit -sivulle.
+                        </button>
+                    </div>
+                </div>
+            </form>
+        
     @endif
   
     </div>
